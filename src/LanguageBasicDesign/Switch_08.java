2456e6210c9dc2b53833aa3ec3bef20e4cd72a21
李朝阳 <cstlizy@hotmail.com>
2014-11-23 14:29:07 +0800

    修改Java类名

--- src/LanguageBasicDesign/Switch_08.java
+++ src/LanguageBasicDesign/Switch_08.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * 按照规定，在高速公路上行使的机动车，超出本车道限速的<10%则处200元罚款；若超出<50%，就要吊销驾驶证。请编写程序根据车速和限速自动判别对该机动车的处理。
  * Created by lzy on 2014/11/22.
  */
-public class Switch_09 {
+public class Switch_08 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in, "utf-8");
         int speed = scanner.nextInt();
